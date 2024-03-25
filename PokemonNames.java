26f9c1f155342c7b9174ed7c3a3d2b07a061b565
Stulargo <114095328+Stulargo@users.noreply.github.com>
2024-03-25 09:53:41 +0100

    Add files via upload

---
+++ PokemonNames.java
@@ -0,0 +1,14 @@
+public class PokemonNames {
+    enum names{
+        ABRA,
+        EEVEE,
+        PIKACHU,
+        CHERUBI,
+        GIRATINA,
+        PIPLUP,
+        CHARIZARD,
+        BUTTERFREE,
+        DUSTOX,
+        ARCEUS
+    }
+}
